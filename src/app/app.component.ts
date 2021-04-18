33e164377aa82e846cbcee6feeaf9024d757035b
Nofil Haroon <54749622+NofilHaroon@users.noreply.github.com>
2021-04-19 01:05:30 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projectv1';
+  title = 'Mimmar';
   constructor(){}
   ngOnInit(): void {
     (function(d, m){
